955359773140e724741faf2dfb5d1ee49153092b
rahulgurnani <rahulgurnani09@gmail.com>
2020-01-05 18:26:00 +0530

    Add code for all permutations of a list

--- all_permutations.py
+++ all_permutations.py
@@ -11,11 +11,23 @@ def all_permutations(ls):
     return res
 
 
+def allPermutations(ls):
+    res = []
+    if len(ls) == 1:
+        return [ls]
+    for i, a in enumerate(ls):
+        permutations = allPermutations(ls[0:i] + ls[i+1:])
+        for permutation in permutations:
+            p = [a] + permutation
+            res.append(p)
+    return res
+
+
 s = input("Enter a number:")
 n = int(s)
 
 
 ls = [i for i in range(0, n)]
-all_permutation = all_permutations(ls)
-print("All permutations:")
-print(all_permutation)
+all_permutation = allPermutations(ls)
+end = [1, 2, 3, 4, 5, 0]
+print(end in all_permutation)
