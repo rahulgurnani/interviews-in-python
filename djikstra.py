95780f597cc0680ace95fe266522aae23bf6e56e
Rahul Gurnani <rahulgurnani09@gmail.com>
2020-08-10 20:02:54 +0530

    Update djikstra.py

--- djikstra.py
+++ djikstra.py
@@ -13,7 +13,7 @@ def shortest_distance(graph, source, dest):
         for neighbor, delta in graph[node]:
             if neighbor in allocated:
                 continue
-        heapq.heappush(distance_heap, (distance+delta, neighbor))
+            heapq.heappush(distance_heap, (distance+delta, neighbor))
 
     return -1
 graph = {(0, 0): [((1, 0), 2), ((0, 1), 5), ((0, 1), 5), ((1, 0), 2)], (1, 0): [((0, 0), 0), ((0, 0), 0), ((1, 1), 2), ((1, 1), 2)], (0, 1): [((0, 0), 0), ((0, 0), 0), ((1, 1), 0), ((1, 1), 0)], (1, 1): [((0, 1), 3), ((1, 0), 0), ((0, 1), 3), ((1, 0), 0)]}
